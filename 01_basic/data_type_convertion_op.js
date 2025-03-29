3bd7374ea297ce8e25abde98608210496e61d8ad
ajay kumar prajapati <ajaykrprajapati01@gmail.com>
2025-03-29 12:03:01 +0000

    convertion of data type

---
+++ 01_basic/data_type_convertion_op.js
@@ -0,0 +1,13 @@
+const abc = 12334
+let city = "Delhi"
+var country = "India"
+isaddress = true
+
+console.table([abc, city, country, isaddress])
+
+let age 
+console.log(age, typeof age)
+console.log(age=Number(age), typeof age)
+
+
+
